ffb838ea37b8ca5e74bda173c4117b3048a051be
Thomas Bridge <tbridge@chicagobooth.edu>
2015-08-24 00:27:57 +0100

    First Java / Netbeans commit

---
+++ src/primefactors/PrimeFactorDisplays.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primefactors;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author thomas
+ */
+public class PrimeFactorDisplays extends JFrame {
+    public PrimeFactorDisplays(String Title) {
+        setSize(200, 200);
+        setTitle(Title);
+    }    
+}
