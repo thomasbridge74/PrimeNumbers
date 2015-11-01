65f7fb52fe9e24829f8b8af22bd7f65066f4e209
Thomas Bridge <tbridge@chicagobooth.edu>
2015-11-01 20:43:23 +0000

    The CLI monitor now uses a couple of threads to monitor the progress of threads.   This can be useful when dealing with numbers with large numbers of prime factors.

--- src/primefactors/PrimeFactorsCLI.java
+++ src/primefactors/PrimeFactorsCLI.java
@@ -16,7 +16,28 @@ public class PrimeFactorsCLI {
         System.out.println("Please enter the number: ");
         Scanner scan = new Scanner(System.in);    
         
-        p = new PrimeFactors(Long.parseLong(scan.nextLine()));
+        p = new PrimeFactors(Long.parseLong(scan.nextLine()), false);
+        // Now I want to run two threads... 
+        
+        // Thread 1 will initiate the calculation process.
+        PrimeFactorsThreadCalculate calculator = new PrimeFactorsThreadCalculate(p);
+        Thread t1 = new Thread(calculator);
+        t1.start();
+        // Thread 2 will monitor the calculation and send output to
+        // user.
+
+        PrimeFactorsMonitorCalculation monitor = new PrimeFactorsMonitorCalculation(p);
+        Thread t2 = new Thread(monitor);
+        t2.start();
+        
+        // Wait for calculation to complete before printing the factors
+        
+        try {
+            t1.join();
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
         p.printFactors();
+        
     }
 }
