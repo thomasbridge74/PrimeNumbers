bf1a2326bb5e47e75d927d5ec590d8aaa357be67
Thomas Bridge <tbridge@chicagobooth.edu>
2015-11-01 14:37:10 +0000

    Moved the factor calculation to a seperate function to be called either by the constructor or can be explicitly invoked.   This is useful for processing large numbers and may also be useful for threading opportunities (to be explored).
    Also, some Javadoc setup in the main PrimeFactors class.

--- src/primefactors/PrimeFactors.java
+++ src/primefactors/PrimeFactors.java
@@ -7,34 +7,88 @@ package primefactors;
 
 import java.util.*;
 
-/**
+/** PrimeFactors is a class with for calculating the PrimeFactors of a number.
+ * It is mostly for the author to play around with various features of the 
+ * Java language.
  *
- * @author thomas
+ * @author Thomas Bridge
+ * @version v0.1
+ * 
  */
 public class PrimeFactors {
     // Use ArrayList to hold the factors... 
     private ArrayList<Long>factors = new ArrayList<Long>();
     private long numberToFactor;
+    private long currentFactorToTest;
+    private boolean completed = false;
     
-    public PrimeFactors(long n) {
-        long currentFactorToTest = 2;
+    /** This constructor method takes the number n as an argument and immediately
+     * calculates the factors of the number
+     * 
+     * @param n The number to calculate the prime factors of.
+     */
+    public PrimeFactors(long n) {        
+        numberToFactor = n; 
+        calculateFactors();
+    }
+    
+    /** This constructor method takes the number n as an argument.  If calculateNow
+     * is set to be false, it will not immediately calculate the factors which means
+     * that calculate factors will need to be explicitly called.
+     * 
+     * @param n The number to calculate the prime factors of.
+     * @param calculateNow if true, calculate the factors immediately (the same as calling with no parameter)
+     */
+    public PrimeFactors(long n, boolean calculateNow) {
         numberToFactor = n;
+        if(calculateNow) {
+            calculateFactors();
+        }
+    }
+    
+    /**
+     *
+     */
+    public void calculateFactors() {
+        long n = numberToFactor;
+        currentFactorToTest =2;
+        // Note - this should only run when currentFactorToTest == 2:
         
-        while(n > 1) {
+        while(n % currentFactorToTest == 0) {
+            factors.add(currentFactorToTest);
+            n = n / currentFactorToTest;
+        }
+        
+        // Take this to 3.
+        
+        currentFactorToTest++;
+        
+        // Now we simply skip all even numbers > 2 as we know 
+        // they are not prime.
+        
+        while(n>1) {
             
             if((n % currentFactorToTest) == 0) {
                 factors.add(currentFactorToTest);
                 n = n / currentFactorToTest;
             } else {
-                currentFactorToTest++;
+                currentFactorToTest+=2;
             }
         }
+        completed = true;
     }
-    
+
+    /**
+     *
+     * @return
+     */
     public long getNumber() {
         return numberToFactor;
     }
     
+    /**
+     *
+     */
     public void printFactors() {
         int i;
         for(i = 0; i< factors.size(); i++) {
@@ -43,6 +97,10 @@ public class PrimeFactors {
         System.out.println();
     }
     
+    /**
+     *
+     * @return
+     */
     public ArrayList<Long> getFactors() {
         return factors;
     }
@@ -59,6 +117,10 @@ public class PrimeFactors {
         return factorList;
     }
     
+    /**
+     *
+     * @return
+     */
     public boolean isPrime() {
         if(factors.size() == 1) {
             return true;
